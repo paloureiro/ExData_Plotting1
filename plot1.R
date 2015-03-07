c07597cd44c0453f7201881c57d5e14f73b121d1
paloureiro <palaplou@gmail.com>
2015-03-07 10:43:50 -0300

    Inserting commentaries in the source code files

--- plot1.R
+++ plot1.R
@@ -2,17 +2,18 @@ setwd("~/GitHub/ExData_Plotting1")
 library(dplyr)
 library(lubridate)
 
-
+# Extracts and loads data from exdata-data-household_power_consumption.zip and household_power_consumption.txt, 
+# respectively
 consumption_df <- read.table(unzip("exdata-data-household_power_consumption.zip","household_power_consumption.txt")
                              ,nrows=2075259,header=T,sep = ";",na.strings = "?")
 
-
+# Parses dates
 consumption_df$Date <- consumption_df$Date %>%
                    parse_date_time("%d%m%Y")
-
+# Selects only 2 days from the data
 consumption_df_excerpt <- consumption_df %>%
         filter(Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02"))
-
+# Plotting
 png(filename = "plot1.png")
 hist(consumption_df_excerpt$Global_active_power,col="red",main="Global Active Power",
      xlab="Global Active Power (kilowatts)")
--- plot4.R
+++ plot4.R
@@ -4,20 +4,22 @@ setwd("~/GitHub/ExData_Plotting1")
 library(dplyr)
 library(lubridate)
 
-
+# Extracts and loads data from exdata-data-household_power_consumption.zip and household_power_consumption.txt, 
+# respectively
 consumption_df <- read.table(unzip("exdata-data-household_power_consumption.zip","household_power_consumption.txt")
                              ,nrows=2075259,header=T,sep = ";",na.strings = "?")
 
-
+# Parses dates
 consumption_df$Date <- consumption_df$Date %>%
         parse_date_time("%d%m%Y")
-
+# Selects only 2 days from the data
 consumption_df_excerpt <- consumption_df %>%
         filter(Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02"))
 
-
+# Merges the "Date" and "Time" columns into a new column called "timestamp"
 consumption_df_excerpt <- mutate(consumption_df_excerpt,timestamp = format(as.POSIXct(paste(Date, Time))))
 
+# After transforming the "timestamp" column to a POSIX format, four graphs are plotted 
 png(filename = "plot4.png")
 par(mfrow= c(2,2))
 
